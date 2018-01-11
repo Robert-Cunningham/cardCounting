5fc2834e9d232458cbad9b838231bc6efe8fa17b
Robert <infinitydeltax@gmail.com>
2018-01-10 22:56:32 -0500

    Got everything working, and settings in a separate dialog

--- src/App.js
+++ src/App.js
@@ -1,51 +1,262 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AppBar from 'material-ui/AppBar'
 import keydown from 'react-keydown';
-
+import Button from 'material-ui/Button';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField'
 
 const rankLookup = ["2","3","4","5","6","7","8","9","10","jack","queen","king","ace"]
 const suitLookup = ["clubs","hearts","diamonds","spades"]
 
+const STATE_PLAY = 1;
+const STATE_CHECK = 2;
+const STATE_DONE = 3;
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <div id="parent">
           <AppBar> Counting Cards </AppBar>
-        <CardSet cards={[{"suit": 2, "rank": 4}, {"suit": 1, "rank": 8}]} cardsToDisplay={5}> </CardSet>
+        <CardController> </CardController>
       </div>
     );
   }
 }
 
-class CardController extends React.Component {
+class CardController extends React.Component { //Toggles between dialog and CardDisplayer
+  constructor(props) {
+    super(props);
+    this.state = {
+      playState: STATE_DONE,
+      cardsToDisplay: 5,
+      rounds: 5,
+      recentScore: 0,
+      correctCount: 10000     
+    }
+    this.done = this.done.bind(this)
+    this.start = this.start.bind(this)
+    this.check = this.check.bind(this)
+    this.setOptions = this.setOptions.bind(this)
+  }
+
+  setOptions(cardsToDisplay, rounds) {
+    this.setState({
+      cardsToDisplay: parseInt(cardsToDisplay),
+      rounds: parseInt(rounds),
+    })
+  }
+
+  done(correctCount) {
+    this.setState((prevstate) => { return ({
+      playState: STATE_CHECK,
+      recentScore: Math.round(((new Date()).getTime() - prevstate.recentStartTime) / prevstate.cardsToDisplay / prevstate.rounds),
+      correctCount: correctCount,
+      guessedCount: 0
+    })})
+  }
+
+  start() {
+    this.setState({
+      playState: STATE_PLAY,
+      recentStartTime: (new Date()).getTime()
+    })
+  }
+
+  check(guessedCount) {
+    this.setState({
+      playState: STATE_DONE,
+      guessedCount: parseInt(guessedCount)
+    })
+  }
+
+  render() {
+    let result = <div> </div>
+    console.log(this.state.correctCount, this.state.guessedCount)
+    if (this.state.correctCount != 10000) {
+      if (this.state.correctCount === this.state.guessedCount) {
+        result = <div>Congratulations! Score: {this.state.recentScore} </div>
+      } else {
+        result = <div> Sorry, incorrect count! ({this.state.correctCount})</div>
+      }
+    }
+    let toDisplay = <div> </div>
+    if (this.state.playState == STATE_DONE) {
+      toDisplay = <PlayDialog play={this.start} setOptions={this.setOptions} cardsToDisplayDefault={this.state.cardsToDisplay} roundsDefault={this.state.rounds} score={this.state.correctCount === this.state.guessedCount ? this.state.recentScore : 0}>
+        {result}        
+      </PlayDialog>
+    } else if (this.state.playState == STATE_PLAY) {
+      toDisplay = <CardDisplayer totalSets={this.state.rounds} cardsToDisplay={this.state.cardsToDisplay} onFinish={this.done} />
+    } else if (this.state.playState == STATE_CHECK) {
+      toDisplay = <CheckDialog done={this.check}/>
+    }
+
+    return (
+      <div>
+        {toDisplay}
+      </div>
+    )
+  }
+}
+
+class CheckDialog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true,
+      count: ""
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    if (e.target.id === "count") {
+      this.setState({count: e.target.value})
+    }
+  }
+
+  render() { return (
+    <div>
+    <Dialog title="Card Counting" open={this.state.open}>
+      <DialogTitle>{"Check your answer"}</DialogTitle>
+      Count: <TextField id="count" onChange={this.handleChange} value={this.state.count}/>
+      <DialogActions>
+        <Button onClick={(e) => {this.props.done(this.state.count)}} color="primary">Check</Button>
+      </DialogActions>
+    </Dialog>
+    </div>
+  )}
+}
+
+class SettingsDialog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+      cardsToDisplay: props.cardsToDisplayDefault,
+      rounds: props.roundsDefault
+    }
+    this.handleSettings = this.handleSettings.bind(this)
+    this.handleOpen = this.handleOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
+    this.save = this.save.bind(this)
+    this.cancel = this.cancel.bind(this)
+  }
+
+  handleSettings(e) {
+    if (e.target.id === "cardsToDisplay") {
+      this.setState({cardsToDisplay: e.target.value});
+    }
+    if (e.target.id === "rounds") {
+      this.setState({rounds: e.target.value});
+    }
+  }
+
+  save(e) {
+    this.props.setOptions(this.state.cardsToDisplay, this.state.rounds)
+    this.setState({open: false})
+  }
+
+  cancel(e) {
+    this.setState({open: false})
+  }
+
+  handleOpen(e) {
+    this.setState({open: true})
+  }
+
+  handleClose(e) {
+    this.setState({open: false})
+  }
+
+  render() {return (
+    <div>
+    <Button onClick={this.handleOpen}>Settings</Button>
+    <Dialog open={this.state.open}>
+      Cards per screen: <TextField id="cardsToDisplay" onChange={this.handleSettings} value={this.state.cardsToDisplay}/>
+      Rounds: <TextField id="rounds" onChange={this.handleSettings} value={this.state.rounds}/>
+      <DialogActions>
+        <Button onClick={this.cancel}>Cancel</Button>
+        <Button onClick={this.save} autoFocus>Save</Button>
+      </DialogActions>
+    </Dialog>
+    </div>
+  )}
+}
+
+class PlayDialog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true,
+    }
+    this.playWithOptions = this.playWithOptions.bind(this)
+  }
+
+  playWithOptions() {
+    this.props.play()
+  }
+
+  
+  render() {
+    return (
+    <Dialog title="Card Counting" open={this.state.open}>
+      <DialogTitle>{"Card Counting with Robert!"}</DialogTitle>
+      {this.props.children}
+      <DialogActions>
+        <SettingsDialog {...this.props}/>
+        <Button onClick={this.playWithOptions} color="primary">Play</Button>
+      </DialogActions>
+    </Dialog>
+    )
+  }
+}
+
+
+class CardDisplayer extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       count : 0,
-      currentCards: []
+      currentCards: [],
+      left : this.props.totalSets
     }
     this.nextSet = this.nextSet.bind(this)
     this.getCountDelta = this.getCountDelta.bind(this)
   }
 
+  componentWillMount() {
+    this.nextSet(undefined)
+  }
+
   getCountDelta(cardSet) {
     return cardSet.reduce((prev, current) => {
-      return prev + (current[0] <= 4 ? 1 : 0) + (current[0] >= 8 ? -1 : 0)
-    })
+      return prev + (current.rank <= 4 ? -1 : 0) + (current.rank >= 8 ? 1 : 0)
+    }, 0)
   }
 
   @keydown("enter")
   nextSet(event) {
+    console.log("enter")
+    if (this.state.left === 0) {
+      this.props.onFinish(this.state.count)
+      return
+    }
     let next = [];
     for (let i = 0; i < this.props.cardsToDisplay; i++) {
-      next.push([Math.random() * 13, Math.random() * 4])
+      next.push({rank: Math.floor(Math.random() * 13), suit: Math.floor(Math.random() * 4)})
     }
     this.setState((prevState) => {
       return ({
         count: prevState.count + this.getCountDelta(next),
+        left: prevState.left -1,
         currentCards: next
       })
     })
@@ -54,30 +265,24 @@ class CardController extends React.Component {
   render() {
     return (<div>
       <CardSet cards={this.state.currentCards}> </CardSet>
-      {this.state.count}
     </div>)
-  }
-
-
-  render() {
-    return <CardSet> </CardSet>
   }
 }
 
 function CardSet(props) {
-  let output = props.cards.map((item) => {
+  let output = props.cards.map((item, index) => {
     return (
-    <li key={getFileName(item.rank, item.suit)}> {<Card type={item}> </Card>} </li>
+    <li key={index}> {<Card type={item}> </Card>} </li>
     )
   })
   return (
-    <ul class="card-set"> {output} </ul>
+    <ul className="card-set"> {output} </ul>
   )
 }
 
 function Card(props) {
   return (
-    <img class="card" src={getFileName(props.type.rank, props.type.suit)} ></img>
+    <img className="card" src={getFileName(props.type.rank, props.type.suit)} ></img>
   )
 }
 
